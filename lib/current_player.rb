fec6bc1bbad1ffeacf41532c9a78f0eb6a9d4550
Alex Ines <lex@blackcat.watch>
2020-05-03 19:28:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,11 @@
 def turn_count(board)
+  count = 0
+  board.each |element| do
+     if element == "X" || element =="O"
+       count += 1
+     end
+  end
+  count
 end
 
 def current_player(board)
